aef8b46ea2c0ab3870a7dc0ab37dcb2411d45754
rafagabidulin <rafagabidulin@gmail.com>
2023-06-05 21:53:28 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import MainPage from './pages/MainPage/MainPage';
 
-export const App = () => <div>Pace'n'Speed</div>;
+export const App = () => (
+  <div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  </div>
+);
