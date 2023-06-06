84a4761a75eff81efb96525977b28c0410f56ae1
rafagabidulin <rafagabidulin@gmail.com>
2023-06-06 22:32:05 +0300

    added props

--- src/components/DistanceButton/DistanceButton.tsx
+++ src/components/DistanceButton/DistanceButton.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-const DistanceButton = ({ distance }) => {
-  return (
-    <div className='px-2'>
-      <Button variant='outline-primary' type='submit'>
-        {distance}
-      </Button>
-    </div>
-  );
-};
+const DistanceButton = ({ distance, onChange }) => (
+  <div className='px-2'>
+    <Button variant='outline-primary' type='submit' onClick={onChange}>
+      {distance}
+    </Button>
+  </div>
+);
 
 export default DistanceButton;
